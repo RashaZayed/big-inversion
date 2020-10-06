b6ea60f8fdf1929ab8bd357f1ef7f095a65baf49
Rasha Zayed <zayed.rasha@outlook.com>
2020-10-06 12:59:53 -0700

    very simple personal Card using functional Components in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <PersonCard lastName='Rendell' firstName='Augisanda'age={29} hairColor='black'/>
+     <PersonCard lastName='Zayed' firstName="Rasha" age={30} hairColor='brown'/>
+     <PersonCard lastName='Dunbabin' firstName='Megan' age={30} hairColor='brown'/>
+     <PersonCard lastName='Abdelhady' firstName='Khaled' age={29} hairColor='brwon'/> 
     </div>
   );
 }
